a5d8584cfa9882b0d8e8bba3d2226d4f437de4cf
helwor <2992845224@qq.com>
2020-09-04 18:11:22 +0800

    完善注释信息

--- src/top/helwor/main/Home.java
+++ src/top/helwor/main/Home.java
@@ -6,6 +6,7 @@ import top.helwor.util.HttpSubmit;
 public class Home {
     public static void main(String[] args){
         HttpSubmit hs = new HttpSubmit();
+        // 运行前先去top.helwor.util.HttpSubmit里设置好自己的信息
         hs.doPost(Api.URL);
     }
 }
