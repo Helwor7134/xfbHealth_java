d400507672893b25e9958a24400e9a55bf355ffc
helwor <2992845224@qq.com>
2020-09-10 16:55:02 +0800

    优化了传参的逻辑

--- src/top/helwor/util/Api.java
+++ src/top/helwor/util/Api.java
@@ -13,9 +13,6 @@ public class Api {
     // 部分上报信息，用于拼接
     public static final String BODY_JSON = "&jqqxbz=&cxzz=1000000000&mqzz=0&whpl=0&yqryjc=0&backdate=&zzbz=&json=true";
 
-    // cookie
-    public static final String COOKIE = "ASP.NET_SessionId=2varrbzsskrski3n50ztye34; imeiticket=eb406c03fcb10eb7b8e17db9a512a77d; sourcetypeticket=5E4A9EA1EBF1402B97E7A09CFD04EA51";
-
     // 本地厂库名
     public static final String PERSON_INFO = "person_info";
 }
