d574090ceec029b84111343dfe4959a76da644a6
helwor <2992845224@qq.com>
2020-09-10 16:56:10 +0800

    完善注释

--- src/top/helwor/util/Api.java
+++ src/top/helwor/util/Api.java
@@ -10,7 +10,7 @@ public class Api {
     // 登陆人信息接口
     public static final String LOGIN_INFO = "http://61.163.246.8:82/User/GetCardInfoByAccountNoParm";
 
-    // 部分上报信息，用于拼接
+    // 部分上报信息，用于拼接，意思是健康，未与高危人员接触
     public static final String BODY_JSON = "&jqqxbz=&cxzz=1000000000&mqzz=0&whpl=0&yqryjc=0&backdate=&zzbz=&json=true";
 
     // 本地厂库名
